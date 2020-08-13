d7735c5cf060b26c17be7092edb75756e65ad548
trevor.zhang <trevor.zhang@ants-city.com>
2020-08-13 17:04:44 +0800

    2

--- day01/main/day01.go
+++ day01/main/day01.go
@@ -3,13 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	sum, sub := dualReturn(5, 1)
-	fmt.Println("sum", sum)
-	fmt.Println("sub", sub)
+	// 1.
+	// sum, sub := dualReturn(5, 1)
+	// fmt.Println("sum", sum)
+	// fmt.Println("sub", sub)
+	// 2.
+	printForm()
 }
 
 func dualReturn(n1 int, n2 int) (int, int) {
 	sum := n1 + n2
 	sub := n1 - n2
 	return sum, sub
+}
+
+func printForm() {
+	fmt.Println("name\tsex\tage\nJohn\tmale\t28")
 }
